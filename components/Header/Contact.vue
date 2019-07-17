6ef5470649015a27c80ff8532b22a122f12a9580
xinhonglee <xinhong1010@outlook.com>
2019-07-17 15:48:24 +0200

    header contact

--- components/Header/Contact.vue
+++ components/Header/Contact.vue
@@ -2,7 +2,7 @@
   <div class="app-header_contact">
     <div class="container v-middle_wrap">
       <div class="v-middle">
-        Adres Regel 1 - Straat - Stad - Telefoonummer: 89838756537 - <strong>In case Of Emergency: 98798635</strong>
+        Address - Zip Code - City - Country - Phone: 89838756537 - <strong>In case Of Emergency: 98798635</strong>
       </div>
     </div>
   </div>
