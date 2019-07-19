a7a1cd254ca8397436e95fa238857acb57e4a4db
xinhonglee <xinhong1010@outlook.com>
2019-07-19 08:38:40 +0200

    update home

--- pages/About.vue
+++ pages/About.vue
@@ -1,17 +1,35 @@
 <template>
-  <div id="page_about" class="main">
-    <div class="container">
-      <div class="headline text-center">
-        <h1>About</h1>
-      </div>
-    </div>
+  <div id="page_home" class="main">
+    <app-page-home-header/>
+    <app-page-home-tenerife/>
+    <app-page-home-about/>
+    <app-page-home-treatments/>
+    <app-page-home-shop/>
+    <app-page-home-news/>
+    <app-page-home-appointment/>
   </div>
 </template>
 
 <script>
   import {paintCommon} from "~/assets/js/animate";
+  import AppPageHomeHeader from "~/components/Pages/Home/SectionHeader";
+  import AppPageHomeTenerife from "../components/Pages/Home/SectionTenerife";
+  import AppPageHomeAbout from "../components/Pages/Home/SectionAbout";
+  import AppPageHomeTreatments from "../components/Pages/Home/SectionTreatments";
+  import AppPageHomeShop from "../components/Pages/Home/SectionShop";
+  import AppPageHomeNews from "../components/Pages/Home/SectionNews";
+  import AppPageHomeAppointment from "../components/Pages/Home/SectionAppointment";
   export default {
     name: "app-page-about",
+    components: {
+      AppPageHomeTenerife,
+      AppPageHomeHeader,
+      AppPageHomeAbout,
+      AppPageHomeTreatments,
+      AppPageHomeShop,
+      AppPageHomeNews,
+      AppPageHomeAppointment
+    },
     data() {
       return {};
     },
