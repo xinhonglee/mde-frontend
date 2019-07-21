122a49bf09440b53d0633b7d6ff8fc737e2b39d5
xinhonglee <xinhong1010@outlook.com>
2019-07-21 19:49:54 +0200

    update treatment

--- components/Pages/Treatments/SectionTreat.vue
+++ components/Pages/Treatments/SectionTreat.vue
@@ -5,126 +5,21 @@
         <b-col md="12">
           <div id="slider">
             <no-ssr>
-              <slick-slide ref="slick" :options="slickOptions">
-                <div class="slider-item">
+              <slick-slide ref="slick" :options="slickOptions" v-if="treatment.length > 0">
+                <div class="slider-item" v-for="(item, index) in treatment" :key="index">
                   <div class="slider-item-image">
                     <img src="~/assets/images/treatments/dental-tooth.png" alt="treatment image">
                   </div>
                   <div class="slider-item-description">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
-                      ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
+                    <p>{{item.description}}</p>
                   </div>
-                  <div class="slider-item-specs">
-                    <div class="slider-item-specs-item">
+                  <div class="slider-item-specs" v-if="item.specs.length > 0" >
+                    <div class="slider-item-specs-item"  v-for="(spec, i) in item.specs" :key="i">
                       <div class="slider-item-specs-item-title">
-                        <h6>Specs 1</h6>
+                        <h6>{{spec.title}}</h6>
                       </div>
                       <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 2</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 3</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="slider-item">
-                  <div class="slider-item-image">
-                    <img src="~/assets/images/treatments/dental-tooth.png" alt="treatment image">
-                  </div>
-                  <div class="slider-item-description">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
-                      ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                  </div>
-                  <div class="slider-item-specs">
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 1</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 2</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 3</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="slider-item">
-                  <div class="slider-item-image">
-                    <img src="~/assets/images/treatments/dental-tooth.png" alt="treatment image">
-                  </div>
-                  <div class="slider-item-description">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor
-                      in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
-                      ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor in
-                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                  </div>
-                  <div class="slider-item-specs">
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 1</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 2</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
-                      </div>
-                    </div>
-                    <div class="slider-item-specs-item">
-                      <div class="slider-item-specs-item-title">
-                        <h6>Specs 3</h6>
-                      </div>
-                      <div class="slider-item-specs-item-description">
-                        <p>In our clinic we offer two kind of treatments. Read more about all our treatments below.</p>
+                        <p>{{spec.description}}</p>
                       </div>
                     </div>
                   </div>
@@ -140,75 +35,18 @@
         </b-col>
       </b-row>
       <b-row>
-        <b-col lg="3" md="6">
+        <b-col lg="3" md="6" v-for="(treatment, index) in treatments" :key="index">
           <div class="treatments-content-item">
             <div class="item-image">
               <img src="~/assets/images/treatments/dental-tooth.png"/>
             </div>
             <div class="item-description">
-              <h5>Prevention</h5>
-              <h6>General</h6>
-              <p>In our clinic we offer two kind of treatments. Read more about all our treatments
-                below. </p>
+              <h5>{{treatment.title}}</h5>
+              <h6>{{treatment.subTitle}}</h6>
+              <p>{{treatment.description}}</p>
             </div>
             <div class="item-button">
-              <nuxt-link active-class="active" to="/treatments/treat?id=1" class="button-extend-normal-round">Read
-                More
-              </nuxt-link>
-            </div>
-          </div>
-        </b-col>
-        <b-col lg="3" md="6">
-          <div class="treatments-content-item">
-            <div class="item-image">
-              <img src="~/assets/images/treatments/dental-tooth.png"/>
-            </div>
-            <div class="item-description">
-              <h5>Restorative</h5>
-              <h6>General</h6>
-              <p>In our clinic we offer two kind of treatments. Read more about all our treatments
-                below. </p>
-            </div>
-            <div class="item-button">
-              <nuxt-link active-class="active" to="/treatments/treat?id=2" class="button-extend-normal-round">Read
-                More
-              </nuxt-link>
-            </div>
-          </div>
-        </b-col>
-        <b-col lg="3" md="6">
-          <div class="treatments-content-item">
-            <div class="item-image">
-              <img src="~/assets/images/treatments/dental-tooth.png"/>
-            </div>
-            <div class="item-description">
-              <h5>Rehabilitation</h5>
-              <h6>General</h6>
-              <p>In our clinic we offer two kind of treatments. Read more about all our treatments
-                below. </p>
-            </div>
-            <div class="item-button">
-              <nuxt-link active-class="active" to="/treatments/treat?id=3" class="button-extend-normal-round">Read
-                More
-              </nuxt-link>
-            </div>
-          </div>
-        </b-col>
-        <b-col lg="3" md="6">
-          <div class="treatments-content-item">
-            <div class="item-image">
-              <img src="~/assets/images/treatments/dental-tooth.png"/>
-            </div>
-            <div class="item-description">
-              <h5>Implants</h5>
-              <h6>General</h6>
-              <p>In our clinic we offer two kind of treatments. Read more about all our treatments
-                below. </p>
-            </div>
-            <div class="item-button">
-              <nuxt-link active-class="active" to="/treatments/treat?id=4" class="button-extend-normal-round">Read
-                More
-              </nuxt-link>
+              <a href="#" class="button-extend-normal-round" @click="loadTreatment(treatment.id)">Read More</a>
             </div>
           </div>
         </b-col>
@@ -229,10 +67,90 @@
           prevArrow: '<div class="slick-prev slick-arrow"></div>',
           nextArrow: '<div class="slick-next slick-arrow"></div>'
         },
+        treatments: [
+          {
+            id: '1',
+            title: 'Prevention',
+            subTitle: 'General',
+            description: `In our clinic we offer two kind of treatments. Read more about all our treatments below.`
+          },
+          {
+            id: '2',
+            title: 'Prevention',
+            subTitle: 'General',
+            description: `In our clinic we offer two kind of treatments. Read more about all our treatments below.`
+          },
+          {
+            id: '3',
+            title: 'Prevention',
+            subTitle: 'General',
+            description: `In our clinic we offer two kind of treatments. Read more about all our treatments below.`
+          },
+          {
+            id: '4',
+            title: 'Prevention',
+            subTitle: 'General',
+            description: `In our clinic we offer two kind of treatments. Read more about all our treatments below.`
+          }
+        ],
+        treatment: [
+          {
+            description: `Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure
+                      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+            specs: [
+              {
+                title: 'Specs 1',
+                description: 'In our clinic we offer two kind of treatments. Read more about all our treatments below.'
+              }
+            ]
+          },
+          {
+            description: `Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure
+                      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+            specs: [
+              {
+                title: 'Specs 1',
+                description: 'In our clinic we offer two kind of treatments. Read more about all our treatments below.'
+              }
+            ]
+          },
+          {
+            description: `Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure
+                      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      Lorem ipsum dolor sit amet, consectetur adipm.Lorem ipsum ea commodo consequat. Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+            specs: [
+              {
+                title: 'Specs 1',
+                description: 'In our clinic we offer two kind of treatments. Read more about all our treatments below.'
+              }
+            ]
+          }
+        ]
       };
     },
     mounted() {
-      $()
+      if (!_.isNil(this.$route.query.id)) {
+        this.loadTreatment(this.$route.query.id);
+      }
+    },
+    methods: {
+      loadTreatment(id) {
+        if (event) {
+          event.preventDefault();
+        }
+        console.log(`load treatment data ${id}`);
+      }
     }
   }
 </script>
