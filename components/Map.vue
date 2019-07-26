6dbd7c622a7cf30557d42d7c6f936667ebe7151b
xinhonglee <xinhong1010@outlook.com>
2019-07-26 12:49:16 +0200

    update googlemap

--- components/Map.vue
+++ components/Map.vue
@@ -9,15 +9,18 @@
         name: "app-google-map",
         async mounted() {
           try {
-            const google = await gmapsInit();
+            let google;
+            if(window['gmapsCallback']) {
+              google = window.google;
+            } else {
+              google = await gmapsInit();
+            }
             const geocoder = new google.maps.Geocoder();
             const map = new google.maps.Map(this.$el);
-
             geocoder.geocode({ address: 'France' }, (results, status) => {
               if (status !== 'OK' || !results[0]) {
                 throw new Error(status);
               }
-
               map.setCenter(results[0].geometry.location);
               map.fitBounds(results[0].geometry.viewport);
             });
