447bd4797d20c9416b3e7692c98b060a79a38520
xinhonglee <xinhong1010@outlook.com>
2019-07-26 14:11:08 +0200

    update location

--- components/Map.vue
+++ components/Map.vue
@@ -1,34 +1,41 @@
 <template>
-    <div id="map"></div>
+  <div id="map"></div>
 </template>
 
 <script>
   import gmapsInit from '~/plugins/gmaps';
 
   export default {
-        name: "app-google-map",
-        async mounted() {
-          try {
-            let google;
-            if(window['gmapsCallback']) {
-              google = window.google;
-            } else {
-              google = await gmapsInit();
-            }
-            const geocoder = new google.maps.Geocoder();
-            const map = new google.maps.Map(this.$el);
-            geocoder.geocode({ address: 'France' }, (results, status) => {
-              if (status !== 'OK' || !results[0]) {
-                throw new Error(status);
-              }
-              map.setCenter(results[0].geometry.location);
-              map.fitBounds(results[0].geometry.viewport);
-            });
-          } catch (error) {
-            console.error(error);
-          }
-      },
-    }
+    name: "app-google-map",
+    async mounted() {
+      try {
+        let google;
+        if (window['gmapsCallback']) {
+          google = window.google;
+        } else {
+          google = await gmapsInit();
+        }
+
+        const uluru = {lat: 28.3650314, lng: -16.3643976};
+        const map = new google.maps.Map(this.$el, {zoom: 17, center: uluru});
+        const marker = new google.maps.Marker({position: uluru, map: map});
+
+        // const geocoder = new google.maps.Geocoder();
+        // const map = new google.maps.Map(this.$el);
+        // const address = `Avenida Marítima 29, 38530 Candelaria, Santa Cruz de Tenerife, Spain`;
+        //
+        // geocoder.geocode({address: address}, (results, status) => {
+        //   if (status !== 'OK' || !results[0]) {
+        //     throw new Error(status);
+        //   }
+        //   map.setCenter(results[0].geometry.location);
+        //   map.fitBounds(results[0].geometry.viewport);
+        // });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  }
 </script>
 
 <style scoped>
